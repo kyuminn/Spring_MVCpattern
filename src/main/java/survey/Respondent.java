275d214954f68bb12ffd65df654adecb16d47b24
user <starkyumin0622@gmail.com>
2021-12-14 17:02:44 +0900

    add survey package

---
+++ src/main/java/survey/Respondent.java
@@ -0,0 +1,12 @@
+package survey;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Respondent { // 응답자의 정보를 담기 위한 클래스
+	private int age;
+	private String location;
+	
+}
