63ef5af5b32f7b54f286deaf1b4292ccf10c46ff
user <starkyumin0622@gmail.com>
2021-12-16 10:15:57 +0900

    add sql file

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -30,7 +30,11 @@ public class LoginController {
 // 보통 form을 보여주는 건 get 요청으로, form 파라미터를 처리하는 것은 post 방식으로 받는다
 // @CookieValue에서 value 속성은 쿠키의 이름 , required 속성은 필수 여뷰 (true가 default)
 	@RequestMapping(method=RequestMethod.GET)
-	public String Form(LoginCommand loginCommand,@CookieValue(value="REMEMBER",required=false)Cookie cookie) { 
+	public String Form(LoginCommand loginCommand,@CookieValue(value="REMEMBER",required=false)Cookie cookie) {
+		if (cookie!=null) {
+			loginCommand.setEmail(cookie.getValue());
+			loginCommand.setRememberEmail(true);
+		}
 		return "login/loginForm";
 	}
 	
@@ -44,14 +48,14 @@ public class LoginController {
 			AuthInfo authInfo = authService.authenticate(loginCommand.getEmail(), loginCommand.getPassword());
 			// 로그인에 성공하면 session 에 로그인 정보 저장
 			session.setAttribute("authInfo", authInfo);
-			Cookie rememberCookie = new Cookie("remember",loginCommand.getEmail());
+			Cookie rememberCookie = new Cookie("REMEMBER",loginCommand.getEmail());
 			rememberCookie.setPath("/");
 			if (loginCommand.isRememberEmail()) {
 				rememberCookie.setMaxAge(60*60*24*30);
 			}else {
 				rememberCookie.setMaxAge(0);
 			}
-			response.addCookie(rememberCookie);
+			response.addCookie(rememberCookie); // 브라우저에 저장하도록 서버에서 쿠키 생성 후 response 객체에 넣어줌
 			return "login/loginSuccess";
 		}catch(IdPasswordNotMatchingException e) {
 			errors.reject("IdPasswordNotMatching"); // 글로벌 에러코드! 특정 파라미터 값이 아닌 객체 전체 검증 (아이디와 패스워드 중 어떤것이 잘못되었는지 사용자는 알 수 없음) 
