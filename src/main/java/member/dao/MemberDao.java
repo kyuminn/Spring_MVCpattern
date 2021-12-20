2566cc05b52007c05e75984f283ec21e2f3b15f7
user <starkyumin0622@gmail.com>
2021-12-20 13:21:47 +0900

    delete unused annotation

--- src/main/java/member/dao/MemberDao.java
+++ src/main/java/member/dao/MemberDao.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Repository;
 import member.rowMapper.MemberRowMapper;
 import member.vo.MemberVo;
 
-@Repository
+
 public class MemberDao {
 	
 	//jdbcTemplate 객체 : jsp에서 사용하던 Connection, PreparedStatement, ResultSet 의 역할을 적절히 대신해줌.
