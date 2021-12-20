3447749e2b3954f25e659e321787ffe598c3ad48
user <starkyumin0622@gmail.com>
2021-12-20 13:42:21 +0900

    add controller annotation

--- src/main/java/controller/LogoutController.java
+++ src/main/java/controller/LogoutController.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 public class LogoutController {
 	
 	@RequestMapping("/logout")
